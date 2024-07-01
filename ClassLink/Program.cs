054040802066f4d6dacd383f53f4269b34c2dfcf
M. Zanibul Haque Shanto <zanibul.shanto@gmail.com>
2024-07-01 16:58:41 +0600

    program.cs

--- ClassLink/Program.cs
+++ ClassLink/Program.cs
@@ -1,18 +1,45 @@
+using ClassLink.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
 
 app.UseHttpsRedirection();
+//app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
+
 
 app.UseAuthorization();
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Logic before executing the next delegate in the Use method");
+    await next.Invoke();
+    Console.WriteLine($"Logic after executing the next delegate in the Use method");
+});
+app.Run(async context => {
+    await context.Response.WriteAsync("Hello from the middleware component.");
+});
 
 app.MapControllers();
-
-
 app.Run();
+
+
+
