09cc492188fa5dd1e0625ea3175fba7b836b7b4d
M. Zanibul Haque Shanto <zanibul.shanto@gmail.com>
2024-07-01 12:21:22 +0600

    Commit 1

---
+++ ClassLink/Extensions/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassLink.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) => 
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder  =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+    }
+}
